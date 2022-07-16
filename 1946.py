f1b184fe659a34efee7528df823b939ada229550
JiIJu <eldk313131@gmail.com>
2022-07-16 22:57:20 +0900

    난이도 1클리어 ㅋ

---
+++ 1946.py
@@ -0,0 +1,24 @@
+# T = int(input())
+
+# for n in range(1,T+1):
+#     N = int(input())
+#     test = ''
+    
+#     for a in range(N):
+#         alpa,num = input().split()
+#         test += alpa*int(num)
+#     print('#{}'.format(n))
+#     for i in range(0,len(test),10):
+#         print(test[i:i+10])
+
+T = int(input())
+
+for i in range(1,T+1):
+    N = int(input())
+    test = ''
+    for n in range(N):
+        alpa,num = input().split()
+        test = test + alpa*int(num)
+    print('#{}'.format(i))
+    for i in range(0,len(test),10):
+        print(test[i:i+10])
